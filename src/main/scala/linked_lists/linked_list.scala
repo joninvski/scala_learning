fe28a39f68f482175558bf86421733cb007f38ee
Joao Trindade <trindade.joao@gmail.com>
2013-02-27 00:07:39 +0000

    Trying to do double lists

--- src/main/scala/linked_lists/linked_list.scala
+++ src/main/scala/linked_lists/linked_list.scala
@@ -1,28 +1,38 @@
-abstract class MyList[A] {
+abstract class MyLinkedList[A] {
   def head: A
-  def tail: MyList[A]
+  def tail: MyLinkedList[A]
   def isEmpty: Boolean
   def length: Int
-  def ::(item: A): MyList[A] = new MyListImpl[A](item, this)
+  def ::(item: A): MyLinkedList[A] = new MyLinkedListImpl[A](item, this)
 }
 
-class MyListImpl[A](val head: A, val tail: MyList[A]) extends MyList[A] {
+abstract class MyDoubleLinkedList[A] extends MyLinkedList[A] {
+  override def ::(item: A): MyDoubleLinkedList[A] = new MyDoubleLinkedListImpl[A](item, this)
+}
+
+class MyDoubleLinkedListImpl[A](val head: A, val tail: MyDoubleLinkedList[A]) extends MyDoubleLinkedList[A] {
   def isEmpty = false
   def length: Int = 1 + tail.length
   override def toString: String = head + " " + tail
 }
 
-object MyListNil extends MyList[Nothing] {
+class MyLinkedListImpl[A](val head: A, val tail: MyLinkedList[A]) extends MyLinkedList[A] {
+  def isEmpty = false
+  def length: Int = 1 + tail.length
+  override def toString: String = head + " " + tail
+}
+
+object MyLinkedListNil extends MyLinkedList[Nothing] {
   def head: Nothing = throw new Exception("head of empty list")
-  def tail: MyList[Nothing] = throw new Exception("tail of empty list")
+  def tail: MyLinkedList[Nothing] = throw new Exception("tail of empty list")
   def isEmpty = true
   def length = 0
   override def toString =  ""
 }
 
-object MyList {
-  def apply[A](items: A*): MyList[A] = {
-    var list: MyList[A] = MyListNil.asInstanceOf[MyList[A]]
+object MyLinkedList {
+  def apply[A](items: A*): MyLinkedList[A] = {
+    var list: MyLinkedList[A] = MyLinkedListNil.asInstanceOf[MyLinkedList[A]]
     for (idx <- 0 until items.length reverse)
       list = items(idx) :: list
     list
@@ -30,5 +40,8 @@ object MyList {
 }
 
 object Main extends App {
-    Console.println("Hello, world!");
+  var list = MyLinkedList("ABC", "WXYZ", "123")
+  var doubleList = MyDoubleLinkedList("ABC", "WXYZ", "123")
+  // 3.14159 :: list
+  Console.println(list);
 }
