521dea01fcd71175b3dd3860681f449cf48f53d4
Joao Trindade <trindade.joao@gmail.com>
2013-03-01 11:25:10 +0000

    Continuing the test for double linked lists

--- src/main/scala/linked_lists/linked_list.scala
+++ src/main/scala/linked_lists/linked_list.scala
@@ -8,12 +8,14 @@ abstract class MyLinkedList[A] {
 
 abstract class MyDoubleLinkedList[A] extends MyLinkedList[A] {
   override def ::(item: A): MyDoubleLinkedList[A] = new MyDoubleLinkedListImpl[A](item, this)
+  var previous: MyDoubleLinkedList[A]
 }
 
-class MyDoubleLinkedListImpl[A](val head: A, val tail: MyDoubleLinkedList[A]) extends MyDoubleLinkedList[A] {
+class MyDoubleLinkedListImpl[A](val head: A,  val tail: MyDoubleLinkedList[A]) extends MyDoubleLinkedList[A] {
   def isEmpty = false
   def length: Int = 1 + tail.length
   override def toString: String = head + " " + tail
+  previous = tail
 }
 
 class MyLinkedListImpl[A](val head: A, val tail: MyLinkedList[A]) extends MyLinkedList[A] {
@@ -39,9 +41,27 @@ object MyLinkedList {
   }
 }
 
+object MyDoubleLinkedListNil extends MyDoubleLinkedList[Nothing] {
+  def head: Nothing = throw new Exception("head of empty list")
+  def tail: MyDoubleLinkedList[Nothing] = throw new Exception("tail of empty list")
+  def previous: MyDoubleLinkedList[Nothing] = throw new Exception("tail of empty list")
+  def isEmpty = true
+  def length = 0
+  override def toString =  ""
+}
+
+object MyDoubleLinkedList {
+  def apply[A](items: A*): MyDoubleLinkedList[A] = {
+    var list: MyDoubleLinkedList[A] = MyDoubleLinkedListNil.asInstanceOf[MyDoubleLinkedList[A]]
+    for (idx <- 0 until items.length reverse)
+      list = items(idx) :: list
+    list
+  }
+}
+
 object Main extends App {
   var list = MyLinkedList("ABC", "WXYZ", "123")
-  var doubleList = MyDoubleLinkedList("ABC", "WXYZ", "123")
+  var doubleList = MyDoubleLinkedList("ola")
   // 3.14159 :: list
   Console.println(list);
 }
